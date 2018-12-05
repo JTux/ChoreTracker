978dafe9085474688517fbeb9421062c664c6eec
Joshua Tucker <thejltucker@gmail.com>
2018-12-05 11:59:28 -0500

    Started working towards group owner/member controllers

---
+++ ChoreTracker.WebMVC/Controllers/OwnerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChoreTracker.WebMVC.Controllers
+{
+    [Authorize(Roles = "Admin,GroupOwner")]
+    public class OwnerController : Controller
+    {
+        // GET: Owner
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
