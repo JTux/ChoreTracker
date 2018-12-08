b13e7e2133fa779558d6122a88455cce9c4623a4
Joshua Tucker <thejltucker@gmail.com>
2018-12-08 15:29:48 -0500

    Added controllers for group owners and members

---
+++ ChoreTracker.WebMVC/Controllers/GroupController.cs
@@ -0,0 +1,31 @@
+﻿using ChoreTracker.Models.GroupModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChoreTracker.WebMVC.Controllers
+{
+    public class GroupController : Controller
+    {
+        // GET: Group
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(GroupCreate model)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
