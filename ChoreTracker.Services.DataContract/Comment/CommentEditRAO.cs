df8ec272c8666bbdd19047f4c5955c4a4fd4d69a
JTux <the1jtux@gmail.com>
2019-01-31 22:52:08 -0500

    Comment editing functional

---
+++ ChoreTracker.Services.DataContract/Comment/CommentEditRAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChoreTracker.Services.DataContract.Comment
+{
+    public class CommentEditRAO
+    {
+        public int CommentId { get; set; }
+        public string Content { get; set; }
+    }
+}
