b9a85c2550d1682f15b59c7f133c65f19c768030
JTux <the1jtux@gmail.com>
2019-02-02 10:40:57 -0500

    Created Reward edit/detail models

---
+++ ChoreTracker.Services.DataContract/Reward/RewardDetailRAO.cs
@@ -0,0 +1,13 @@
+﻿namespace ChoreTracker.Services.DataContract.Reward
+{
+    public class RewardDetailRAO
+    {
+        public int RewardId { get; set; }
+
+        public string RewardName { get; set; }
+
+        public string RewardDescription { get; set; }
+
+        public int RewardCost { get; set; }
+    }
+}
